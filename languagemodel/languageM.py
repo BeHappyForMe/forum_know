6ab756ffaa52bfd1246e7055e5c7161c2dd0c7fa
BeHappyForMe <563987805@qq.com>
2020-02-11 18:06:42 +0800

    fix the synonymous

--- languagemodel/languageM.py
+++ languagemodel/languageM.py
@@ -54,7 +54,7 @@ def preprocess_data(args):
     return VOCAB_SIZE,train_iter,dev_iter,test_iter
 
 def train(args,model,train_iter,val_iter,loss_fn,VOCAB_SIZE):
-    LOG_FILE = "language_model.log"
+    LOG_FILE = "language_model_GRU.log"
     tb_writer = SummaryWriter('./runs')
 
     t_total = args.num_epoch * len(train_iter)
@@ -96,8 +96,8 @@ def train(args,model,train_iter,val_iter,loss_fn,VOCAB_SIZE):
             # 防止梯度爆炸，设定阈值，当梯度大于阈值时，更新的梯度为阈值
 
             tr_loss += loss.item()
-            if (i+1) % 1000 == 0:
-                loss_scalar = (tr_loss-logg_loss) / 1000
+            if (i+1) % 200 == 0:
+                loss_scalar = (tr_loss-logg_loss) / 200
                 logg_loss = tr_loss
                 with open(LOG_FILE, "a") as fout:
                     fout.write("epoch: {}, iter: {}, loss: {},learn_rate: {}\n".format(epoch, i, loss_scalar,scheduler.get_lr()[0]))
@@ -105,7 +105,7 @@ def train(args,model,train_iter,val_iter,loss_fn,VOCAB_SIZE):
                 tb_writer.add_scalar("learning_rate",scheduler.get_lr()[0],global_step)
                 tb_writer.add_scalar("loss",loss_scalar,global_step)
 
-            if i % 10000 == 0:
+            if (i+1) % 1000 == 0:
                 val_loss = evaluate(args,model, val_iter,loss_fn,VOCAB_SIZE)
                 with open(LOG_FILE, "a") as fout:
                     print("epoch: {}, iteration: {}, val_loss: {}\n".format(
@@ -116,8 +116,9 @@ def train(args,model,train_iter,val_iter,loss_fn,VOCAB_SIZE):
                 if len(val_losses) == 0 or val_loss < min(val_losses):
                     # 如果比之前的loss要小，就保存模型
                     print("best model, val loss: ", val_loss)
-                    torch.save(model.state_dict(), "lm-best.th")
-                val_losses.append(val_loss)  # 保存每10000次迭代后的验证集损失损失
+                    torch.save(model.state_dict(), "lm-best-GRU.th")
+                val_losses.append(val_loss)  # 保存每1000次迭代后的验证集损失损失
+                tb_writer.add_scalar("preplexity", np.exp(val_loss), global_step)
 
 def repackage_hidden(h):
     """Wraps hidden states in new Tensors, to detach them from their history."""
@@ -152,7 +153,7 @@ def evaluate(args,model, data,loss_fn,VOCAB_SIZE):
             # total_count先计算验证集样本的单词总数，一个样本有50个单词，一个batch32个样本
             # np.multiply(*data.size()) =50*32=1600
             total_loss += loss.item() * np.multiply(*data.size())
-    # 每次batch平均后的损失乘以每次batch的样本的总的单词数 = 一次batch总的损失
+            # 每次batch平均后的损失乘以每次batch的样本的总的单词数 = 一次batch总的损失
 
     loss = total_loss / total_count  # 整个验证集总的损失除以总的单词数
     model.train()  # 训练模式
@@ -165,8 +166,8 @@ def main():
     parse.add_argument("--batch_size", default=16, type=int)
     parse.add_argument("--do_train",default=True, action="store_true", help="Whether to run training.")
     parse.add_argument("--do_eval", default=True, action="store_true", help="Whether to run training.")
-    parse.add_argument("--learnning_rate", default=5e-4, type=float)
-    parse.add_argument("--num_epoch", default=2, type=int)
+    parse.add_argument("--learnning_rate", default=1e-4, type=float)
+    parse.add_argument("--num_epoch", default=5, type=int)
     parse.add_argument("--max_vocab_size",default=50000,type=int)
     parse.add_argument("--embed_size",default=300,type=int)
     parse.add_argument("--warmup_steps", default=0, type=int, help="Linear warmup over warmup_steps.")
@@ -181,7 +182,7 @@ def main():
 
     VOCAB_SIZE, train_iter, dev_iter, test_iter = preprocess_data(args)
 
-    model = RNNModel('LSTM',VOCAB_SIZE,args.embed_size,args.hidden_size,args.num_layers)
+    model = RNNModel('GRU',VOCAB_SIZE,args.embed_size,args.hidden_size,args.num_layers)
     model.to(device)
 
     loss_fn = nn.CrossEntropyLoss()  # 交叉熵损失
@@ -189,10 +190,13 @@ def main():
         train(args,model,train_iter,dev_iter,loss_fn,VOCAB_SIZE)
 
     if args.do_eval:
-        model.load_state_dict(torch.load('lm-best.th'))
+        model.load_state_dict(torch.load('lm-best-GRU.th'))
         model.to(device)
 
         test_loss = evaluate(args,model,test_iter,loss_fn,VOCAB_SIZE)
+        LOG_FILE = "language_model_GRU.log"
+        with open(LOG_FILE,'a') as fout:
+            fout.write("test perplexity: {} ".format(np.exp(test_loss)))
         print("perplexity: ", np.exp(test_loss))
 
 if __name__ == '__main__':
